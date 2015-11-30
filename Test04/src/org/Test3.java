fe19360243244fa5471acd08fcd3b14f69003065
nguyenanh1985 <nguyenanh01011985@gmail.com>
2015-11-30 22:39:48 +0700

    update 1

---
+++ Test04/src/org/Test3.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package org;
+
+/**
+ * @author nhanh
+ *
+ */
+public class Test3 {
+
+	/**
+	 * 
+	 */
+	public Test3() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
