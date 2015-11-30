bcf881a4d515ccff21bb4f94f48c4c4e2304aa52
nguyenanh1985 <nguyenanh01011985@gmail.com>
2015-11-30 22:55:34 +0700

    test

---
+++ Test06/src/aa/A.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package aa;
+
+/**
+ * @author nhanh
+ *
+ */
+public class A {
+
+	/**
+	 * 
+	 */
+	public A() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
